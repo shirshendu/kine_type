211abddaa44c6596f6f631f1d17cdd219c24a1fa
Shirshendu Mukherjee <shirshendu.mukherjee.88@gmail.com>
2015-02-18 20:21:15 +0530

    Reduce transition durations to 300ms

--- public/editor/preview_export/scripts/loadPresentation.js
+++ public/editor/preview_export/scripts/loadPresentation.js
@@ -3,7 +3,8 @@ var loadPresentation = function() {
 	var config = JSON.parse(localStorage.getItem('preview-config'));
 
 	if (presentation) {
-		document.querySelector('#kine_ppt').innerHTML = presentation;
+    document.querySelector('#kine_ppt').innerHTML = presentation;
+    document.querySelector('#impress').setAttribute('data-transition-duration', 300);
 	//	document.body.className = config.surface + " " + document.body.className;
 	}
 };
