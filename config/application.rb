fcb7c2df29a3194928c3b6e62f19eb06af7898a6
Shirshendu Mukherjee <shirshendu.mukherjee.88@gmail.com>
2015-03-04 12:25:27 +0530

    Add content length header for progress bars

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module KineType
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.use Rack::ContentLength
   end
 end
